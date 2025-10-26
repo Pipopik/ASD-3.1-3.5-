560d736c554cf876eafd610f578934ce2ff93955
Pipopik <boez.egor67@gmail.com>
2025-10-26 21:35:56 +0300

    Create Bubble Sort.py

---
+++ Bubble Sort.py
@@ -0,0 +1,12 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+# Пример использования
+test_array = [64, 34, 25, 12, 22, 11, 90]
+print("Исходный массив:", test_array)
+print("Отсортированный массив:", bubble_sort(test_array.copy()))
